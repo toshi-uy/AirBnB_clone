876d9d8e1d1671381cd5538c42b3d086cfb97bbd
toshi-uy <s_borgia@hotmail.com>
2021-06-25 12:26:08 -0300

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
 	def __str__(self):
 		""" String Representation """
         return ("[{}] ({}) {}".format(self.__class__.__name__, 
-									  self.id, self.__dict__))
+		self.id, self.__dict__))
 
 	def save(self):
 		""" updates the public instance attribute with the current datetime """
