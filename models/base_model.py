73304df1299150c88f449be3066a9065656433eb
toshi-uy <s_borgia@hotmail.com>
2021-06-25 12:23:24 -0300

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Base Model for AirBnB
 """
 import datetime
 import json
+from typing import NewType
 from uuid import uuid4
 
 
@@ -21,3 +22,16 @@ class BaseModel:
 		""" String Representation """
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
+
+	def save(self):
+		""" updates the public instance attribute with the current datetime """
+		today = datetime.today()
+		self.updated_at = today
+
+	def to_dict(self):
+		""" returns a dictionary containing all keys/values of the instance"""
+		new = self.__dict__.copy()
+		new["created_at"] = self.created_at.isoformat()
+		new["updated_at"] = self.updated_at.isoformat()
+		new["__class__"] = self.__class__.__name__
+		return new
