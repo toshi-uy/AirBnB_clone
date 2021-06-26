bb092a5ff5bd87feab2a017c902482ac9115b261
toshi-uy <s_borgia@hotmail.com>
2021-06-26 17:55:28 -0300

    added engine and filestorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+convert the dictionary representation to a JSON string
+"""
+import json
+from models.base_model import BaseModel
+
+
+class Filestorage:
+    """
+    serializes instances to a JSON file and deserializes
+    JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = str(obj.__class__.__name__) + "." + str(obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        new = {}
+        for key, values in self.__objects.items():
+            new[key] = values.to_dict()
+
+        with open(self.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(new, f)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        Only if the file exists, else do nothing, no exception
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                objects = json.load(f)
+                for key in objects:
+                    self.__objects[key] = BaseModel(**object[key])
+        except FileNotFoundError:
+            pass
