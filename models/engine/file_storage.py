85f5596e174916bc1a0e5922192e4f9d9778e96b
toshi-uy <s_borgia@hotmail.com>
2021-06-29 15:37:57 -0300

    comitted back

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 convert the dictionary representation to a JSON string
 """
 import json
+from models import city
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -22,20 +23,20 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = str(obj.__class__.__name__) + "." + str(obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new = {}
-        for key, values in FileStorage.__objects.items():
+        for key, values in self.__objects.items():
             new[key] = values.to_dict()
 
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             json.dump(new, f)
 
     def reload(self):
@@ -47,11 +48,9 @@ class FileStorage:
                    'State': State, 'Amenity': Amenity, 'Place': Place,
                    'Review': Review}
         try:
-                with open(self.__file_path, 'r') as f:
-                    objects = json.load(f)
-                    for keys, values in objects.items():
-                        temp = keys.split('.')
-                        new = classes[temp[0]](**values)
-                        self.new(new)
+            with open(self.__file_path, 'r') as f:
+                objects = json.load(f)
+            for key in objects:
+                self.__objects[key] = classes[objects[key]['__class__']](**objects[key])
         except FileNotFoundError:
             pass
