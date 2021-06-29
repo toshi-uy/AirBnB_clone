4a7d8b4757a6b6a891e9624a530f3e58466581fb
Miguel22247 <minipachru@gmail.com>
2021-06-29 11:12:23 -0300

    fixed place module

--- models/place.py
+++ models/place.py
@@ -6,6 +6,14 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """Place module"""
 
-    place_id = ""
+    city_id = ""
     user_id = ""
-    text = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
