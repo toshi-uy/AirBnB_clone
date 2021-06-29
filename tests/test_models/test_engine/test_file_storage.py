e98ec05025ccfa1aef3c3ad0cd4dfe39f5068ea8
Miguel22247 <minipachru@gmail.com>
2021-06-29 12:12:39 -0300

    added test for file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for FileStorage"""
+
+import models
+from models.engine.file_storage import FileStorage
+import os
+import unittest
+
+
+class TestFile_Storage(unittest.TestCase):
+	"""class TestFile_storage""""
