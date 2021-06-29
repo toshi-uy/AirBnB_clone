7da44157b93c84e468be41636fd4adae971e0bfa
Miguel22247 <minipachru@gmail.com>
2021-06-29 10:38:47 -0300

    added test to review module

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""test of review module"""
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
+import pep8
+import inspect
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestDocs(unittest.TestCase):
+    """Base model document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Class docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+class TestReview(unittest.TestCase):
+        """Test Review Class"""
+        self.assertEqual(Review.place_id, "")
+        self.assertEqual(Review.user_id, "")
+        self.assertEqual(Review.text, "")
+        self.assertTrue(issubclass(Review, BaseModel))
