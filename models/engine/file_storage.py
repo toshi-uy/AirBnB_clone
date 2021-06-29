0e126c9e6b8ac1dd8f64e1466db0659264f0bb05
toshi-uy <s_borgia@hotmail.com>
2021-06-29 14:35:41 -0300

    added imports to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,13 @@
 convert the dictionary representation to a JSON string
 """
 import json
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class Filestorage:
