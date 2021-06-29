d6595c8a81f7ea722dfe41fbaac942579fd422bb
Miguel22247 <minipachru@gmail.com>
2021-06-29 11:26:33 -0300

    added test files to complete

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""test the Amenity method"""
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+import pep8
+import inspect
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestDocs(unittest.TestCase):
+    """Base model document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Class docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+class TestAmenity(unittest.TestCase):
+    """Test the Amenity module"""
