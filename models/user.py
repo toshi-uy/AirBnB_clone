5f64a6576749ab91fa75b273a492a648752ebd0d
Miguel22247 <minipachru@gmail.com>
2021-06-29 09:32:21 -0300

    added user module

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""User module"""
+from models.base_model import BaseModel
+
+class UserModel(BaseModel):
+    """Creates the user model"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
