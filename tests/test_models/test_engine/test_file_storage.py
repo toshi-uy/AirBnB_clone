e6f696b5f1df462e0568d08623eac4b76ac9efb6
Miguel22247 <minipachru@gmail.com>
2021-06-29 12:29:46 -0300

    deleted test_doc

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,20 +16,3 @@ class Test_pep8(unittest.TestCase):
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
-
-
-class TestDocs(unittest.TestCase):
-    """Base model document tests"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Testing class"""
-        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
-
-    def test_module_docstring(self):
-        """module docstring length"""
-        self.assertTrue(len(BaseModel.__doc__) >= 1)
-
-    def test_class_docstring(self):
-        """Class docstring length"""
-        self.assertTrue(len(BaseModel.__doc__) >= 1)
