00e7f592bd13082582b580699d4dd553c2b604c8
Miguel22247 <minipachru@gmail.com>
2021-06-29 11:09:51 -0300

    added test_place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""test the place method"""
+import unittest
+from models.base_model import BaseModel
+from models.place import Place
+import pep8
+import inspect
+
+
+class Test_pep8(unittest.TestCase):
+    """pep8 test cases class"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestDocs(unittest.TestCase):
+    """Base model document tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Testing class"""
+        cls.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_module_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Class docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+class TestPlace(unittest.TestCase):
+    """Test the Place module"""
+
+    def test_class(self):
+        """Test class"""
+        self.assertEqual(Place.city_id, "")
+        self.assertEqual(Place.user_id, "")
+        self.assertEqual(Place.name, "")
+        self.assertEqual(Place.description, "")
+        self.assertEqual(Place.number_rooms, 0)
+        self.assertEqual(Place.number_bathrooms, 0)
+        self.assertEqual(Place.max_guest, 0)
+        self.assertEqual(Place.price_by_night, 0)
+        self.assertEqual(Place.latitude, 0.0)
+        self.assertEqual(Place.longitude, 0.0)
+        self.assertEqual(Place.amenity_ids, [])
+        self.assertTrue(issubclass(Place, BaseModel))
