30436d14cff9dacd8fb9c49c73bc4ef692736cc8
toshi-uy <s_borgia@hotmail.com>
2021-06-29 20:16:30 -0300

    added corrections to update

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Console that contains the entry point of the command interpreter"""
+"""Console that contains the entry point of the comm interpreter"""
 import cmd
 import json
 from models.amenity import Amenity
@@ -15,7 +15,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """
     Holberton AirBnB clone console
-    contains the entry point of the command interpreter
+    contains the entry point of the comm interpreter
     quit and EOF to exit the program
     help dispays help
     an empty line + ENTER doesn't execute anything
@@ -23,6 +23,8 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     classes = ["BaseModel", "User", "State", "City",
                "Amenity", "Place", "Review"]
+    attributes = ["updated_at", "created_at", "id"]
+    specs = ["\'", "\""]
 
     def do_EOF(self, line):
         """Exits on EOF"""
@@ -55,18 +57,18 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """Prints the string representation of an instance
         based on the class name"""
-        command = line.split()
+        comm = line.split()
         if not line:
             print("** class name missing **")
             return
-        elif command[0] not in self.classes:
+        elif comm[0] not in self.classes:
             print("** class doesn't exist **")
             return
-        elif len(command) == 1:
+        elif len(comm) == 1:
             print("** instance id missing **")
             return
-        if len(command) == 2:
-            new_item = "{}.{}".format(command[0], command[1])
+        if len(comm) == 2:
+            new_item = "{}.{}".format(comm[0], comm[1])
             if new_item not in storage.all():
                 print("** no instance found **")
                 return
@@ -75,18 +77,18 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """method to delete an instance based on the class name and id"""
-        command = line.split()
+        comm = line.split()
         if not line:
             print("** class name missing **")
             return
-        elif command[0] not in self.classes:
+        elif comm[0] not in self.classes:
             print("** class doesn't exist **")
             return
-        elif len(command) == 1:
+        elif len(comm) == 1:
             print("** no instance found **")
             return
         else:
-            new_item = "{}.{}".format(command[0], command[1])
+            new_item = "{}.{}".format(comm[0], comm[1])
             if new_item not in storage.all():
                 print("** no instance found **")
             else:
@@ -112,28 +114,37 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, line):
         """Updates an instance based on the class name and id"""
-        command = line.split()
+        comm = line.split()
         if not line:
             print("** class name missing **")
-        elif command[0] not in self.classes:
+        elif comm[0] not in self.classes:
             print("** class doesn't exist **")
             return
-        elif len(command) == 1:
+        elif len(comm) == 1:
             print("** instance id missing **")
             return
-        elif command[0] + "." + command[1] not in storage.all().keys():
+        elif comm[0] + "." + comm[1] not in storage.all().keys():
             print("** no instance found **")
             return
-        elif len(command) == 2:
+        elif len(comm) == 2:
             print("** attribute name missing **")
             return
-        elif len(command) == 3:
+        elif len(comm) == 3:
             print("** value missing **")
             return
         else:
-            new_obj = storage.all().get(command[0] + "." + command[1])
-            setattr(new_obj, command[2], command[3][1, -1])
-            new_obj.save()
+            object = storage.all()
+            key = comm[0] + "." + comm[1]
+            if key in object:
+                if comm[2] not in self.attributes:
+                    if comm[3][0] in self.specs and comm[3][-1] in self.specs:
+                        setattr(object[key], comm[2], str(comm[3][1: -1]))
+                    else:
+                        setattr(object[key], comm[2], str(comm[3]))
+                    storage.save()
+            else:
+                print("** no instance found **")
+                return
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
