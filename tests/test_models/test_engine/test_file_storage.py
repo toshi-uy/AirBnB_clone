f0e37ed05f250c97043c51c9025d2e03a7f60a4e
Miguel22247 <minipachru@gmail.com>
2021-06-29 12:36:11 -0300

    added test cases to file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """test the Amenity method"""
 import unittest
-import models
+from models.base_model import BaseModel
 from models.engine.file_storage import Filestorage
+from models.amenity import Amenity
+from models.city import City
+from datetime import datetime
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 import os
 import pep8
 import inspect
@@ -16,3 +23,25 @@ class Test_pep8(unittest.TestCase):
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+class Test_Filestorage(unittest.TestCase):
+    def test_docstring(self):
+        """module docstring length"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_executable_file(self):
+        """ function test_executable_file """
+        is_read_true = os.access("models/engine/file_storage.py", os.R_OK)
+        self.assertTrue(is_read_true)
+        is_write_true = os.access("models/engine/file_storage.py", os.W_OK)
+        self.assertTrue(is_write_true)
+        is_exec_true = os.access("models/engine/file_storage.py", os.X_OK)
+        self.assertTrue(is_exec_true)
+
+    def test_is_an_instance(self):
+        """ function test_is_an_instance """
+        my_module = Filestorage()
+        self.assertIsInstance(my_module, Filestorage)
+
+if __name__ == '__main__':
+    unittest.main()
