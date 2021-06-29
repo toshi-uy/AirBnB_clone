ceb23fb18715d3304ee40ba9d799281a1a569e30
Miguel22247 <minipachru@gmail.com>
2021-06-29 15:36:42 -0300

    returned to first state

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,9 +38,9 @@ class Filestorage:
         Only if the file exists, else do nothing, no exception
         """
         try:
-            with open(self.__file_path, 'r') as f:
-                objects = json.load(f)
-                for key in objects:
-                    self.__objects[key] = BaseModel(**objects[key])
+                with open(self.__file_path, 'r') as f:
+                    objects = json.load(f)
+                    for key in objects:
+                        self.__objects[key] = BaseModel(**objects[key])
         except FileNotFoundError:
             pass
