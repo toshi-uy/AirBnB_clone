b8579dd573c6f2a6121c493d3f114df2432ab3cf
toshi-uy <s_borgia@hotmail.com>
2021-06-29 15:05:09 -0300

    changed logic

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,7 +50,11 @@ class Filestorage:
         try:
             with open(self.__file_path, 'r') as f:
                 objects = json.load(f)
-            for key in objects:
-                self.__objects[key] = classes[objects[key]['__class__']](**objects[key])
+            for keys, values in objects.items():
+                temp = keys.split('.')
+                new = classes[temp[0]](**values)
+                self.new(new)
+            # for key in objects:
+            #     self.__objects[key] = classes[objects[key]['__class__']](**objects[key])
         except FileNotFoundError:
             pass
