e63d3109d0f541d9876ab2f000cb554809bf521f
toshi-uy <s_borgia@hotmail.com>
2021-06-30 12:42:07 -0300

    added save test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """test the FileStorage method"""
-import unittest
+import json
+from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.engine.file_storage import FileStorage
-from models import storage
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
 from os import path
-import json
 import pep8
+import unittest
 
 
 class Test_pep8(unittest.TestCase):
@@ -82,6 +87,35 @@ class Test_FileStorage(unittest.TestCase):
     def test_reload_with_arg(self):
         with self.assertRaises(TypeError):
             storage.reload(None)
+
+    def test_save(self):
+        '''Test save method'''
+        objs = storage
+        new_base = BaseModel()
+        new_user = User()
+        new_state = State()
+        new_place = Place()
+        new_city = City()
+        new_amenity = Amenity()
+        new_review = Review()
+        objs.new(new_base)
+        objs.new(new_user)
+        objs.new(new_state)
+        objs.new(new_place)
+        objs.new(new_city)
+        objs.new(new_amenity)
+        objs.new(new_review)
+        objs.save()
+        save_text = ""
+        with open("file.json", "r") as f:
+            save_text = f.read()
+            self.assertIn("BaseModel." + new_base.id, save_text)
+            self.assertIn("User." + new_user.id, save_text)
+            self.assertIn("State." + new_state.id, save_text)
+            self.assertIn("Place." + new_place.id, save_text)
+            self.assertIn("City." + new_city.id, save_text)
+            self.assertIn("Amenity." + new_amenity.id, save_text)
+            self.assertIn("Review." + new_review.id, save_text)
 
 if __name__ == '__main__':
     unittest.main()
