3d576484c2cc86f0ef8af8fcad39c16474b2fc31
Miguel22247 <minipachru@gmail.com>
2021-06-30 16:06:35 -0300

    added test create

--- tests/test_console.py
+++ tests/test_console.py
@@ -55,7 +55,15 @@ class Test_Console(unittest.TestCase):
         out = ""
         with patch("sys.stdout", new=StringIO()) as salida:
             self.assertFalse(HBNBCommand().onecmd("\n"))
-            self.assertEqual(out, salida.getvalue().strip())   
+            self.assertEqual(out, salida.getvalue().strip())
+    
+    def test_help_create(self):
+        out = "Creates a new instance of BaseModel,\
+        saves it (to the JSON file) and prints the id.\
+        Ex: $ create BaseModel"
+        with patch("sys.stdout", new=StringIO()) as salida:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(out, salida.getvalue().strip())
 
 
     def setUp(self):
