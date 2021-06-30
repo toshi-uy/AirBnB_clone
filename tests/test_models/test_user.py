a1fa271041bd246f3ecc2ac91164fedc39789d67
Miguel22247 <minipachru@gmail.com>
2021-06-30 10:33:44 -0300

    added holberton passed test (example)

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,7 +4,7 @@ import unittest
 from models.base_model import BaseModel
 from models.user import User
 import inspect
-import pep8
+from models import storage
 
 
 class Test_pep8(unittest.TestCase):
@@ -46,12 +46,30 @@ class Test_user(unittest.TestCase):
         self.assertTrue(issubclass(User, BaseModel))
     
     def test_class1(self):
-        """Test class"""
-        self.assertEqual(User.email, "2780@holbertonschool.com")
-        self.assertEqual(User.password, "1234")
-        self.assertEqual(User.first_name, "Miguel")
-        self.assertEqual(User.last_name, "")
-        self.assertTrue(issubclass(User, BaseModel))
+        """test user class"""
+        all_objs = storage.all()
+        print("-- Reloaded objects --")
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+            print(obj)
+
+        print("-- Create a new User --")
+        my_user = User()
+        my_user.first_name = "Betty"
+        my_user.last_name = "Holberton"
+        my_user.email = "airbnb@holbertonshool.com"
+        my_user.password = "root"
+        my_user.save()
+        print(my_user)
+
+        print("-- Create a new User 2 --")
+        my_user2 = User()
+        my_user2.first_name = "John"
+        my_user2.email = "airbnb2@holbertonshool.com"
+        my_user2.password = "root"
+        my_user2.save()
+        print(my_user2)
+
     
     def test_instance(self):
         our_user = User()
