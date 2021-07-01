5f76f03c2d6c4e3a086def3ece1fe0f203a7fd05
Miguel22247 <minipachru@gmail.com>
2021-07-01 20:02:08 -0300

    added some tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -94,7 +94,123 @@ class Test_Console(unittest.TestCase):
         """Set up tests."""
         storage.reload()
 
-    def test_exit(self):
-        """Tests the exit command"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertTrue(HBNBCommand().onecmd("quit"))
+    def test_create_BaseModel(self):
+        """ Test create a BaseModel """
+        out = "<class 'str'>"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
+            xd = str(type(f.getvalue().strip()))
+            self.assertEqual(out, xd)
+
+    def test_create_User(self):
+        """ Test create a User """
+        out = "<class 'str'>"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create User"))
+            xd = str(type(f.getvalue().strip()))
+            self.assertEqual(out, xd)
+
+    def test_create_amenity(self):
+        """ Test create a Amenity """
+        out = "<class 'str'>"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create Amenity"))
+            xd = str(type(f.getvalue().strip()))
+            self.assertEqual(out, xd)
+
+    def test_create_city(self):
+        """ Test create a City """
+        out = "<class 'str'>"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create City"))
+            xd = str(type(f.getvalue().strip()))
+            self.assertEqual(out, xd)
+
+    def test_create_error(self):
+        """ Test create a create class error """
+        out = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create asdas"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_create_error_two(self):
+        """ Test only create without class """
+        out = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_show_error(self):
+        """ Test only show error """
+        out = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show asd"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_show_only(self):
+        """ Test only show """
+        out = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_show_class_only(self):
+        """ Test only class show """
+        out = "** instance id missing **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show BaseModel"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_show_class_id_error(self):
+        """ Test id error """
+        out = "** no instance found **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show BaseModel asdasd223"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_destroy_only(self):
+        """ Test test_destroy_only"""
+        out = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_destroy_error(self):
+        """ Test destroy class error """
+        out = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy asd"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_destroy_class_only(self):
+        """ Test only show """
+        out = "** instance id missing **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy BaseModel"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_destroy_id_error(self):
+        """ Test id error """
+        out = "** no instance found **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy BaseModel asda231"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_all_class(self):
+        """ Test all class error """
+        out = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("all asdas"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_update_class(self):
+        """ Test update class error """
+        out = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("update asdas"))
+            self.assertEqual(out, f.getvalue().strip())
+
+    def test_quit(self):
+        """ checks the exit command"""
+        self.assertTrue(HBNBCommand().onecmd("quit"))
+
